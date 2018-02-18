20b47d914deac997b72ff78ffdc1af05d6f6fefc
Kevin Cala Sánchez <kevin@kcsystem.es>
2018-02-18 21:29:33 +0100

    Delete Useless

--- lotto/nyah.php
+++ lotto/nyah.php
@@ -7,49 +7,6 @@
  * Date: 16/02/2018
  * Time: 21:48
  */
-//$db = array(
-//    array("Abradolf" => 3),
-//    array("AddNegro" => 80),
-//    array("Adrian" => 10),
-//    array("Aitor Astorga" => 60),
-//    array("Alvarino20" => 10),
-//    array("ApioD3stroyer" => 2),
-//    array("Cebolla_OldMan" => 20),
-//    array("Cipriano Martos" => 400),
-//    array("Cristobal Montoro" => 5),
-//    array("David Gonzalez Caraballo" => 80),
-//    array("Dios dedado de Asgardia" => 200),
-//    array("Echenique Rojo" => 40),
-//    array("Eliteloir" => 10),
-//    array("Ignacion66" => 2),
-//    array("Iker" => 10),
-//    array("Inigo Zoco" => 60),
-//    array("Irony" => 250),
-//    array("Katakuri" => 1),
-//    array("Lenil" => 10),
-//    array("Lord Stalin de Asgardia" => 600),
-//    array("Luciano Destroyer" => 20),
-//    array("M.Izeta" => 80),
-//    array("Magotp" => 250),
-//    array("Manema17" => 3),
-//    array("Marc Gomez Perez" => 40),
-//    array("Nacho Fatule" => 10),
-//    array("Nepomu" => 30),
-//    array("Penultimo" => 27),
-//    array("QuoVadix" => 2),
-//    array("Rafnarac" => 6),
-//    array("Rick Sanchez" => 20),
-//    array("Sambar" => 25),
-//    array("San Cid El Pasificador" => 20),
-//    array("Sir Stalin" => 10),
-//    array("Sr. Luis Vera" => 100),
-//    array("Toti Cid" => 5),
-//    array("Vincis Pioleteado" => 200),
-//    array("???? ?????????" => 60)
-//);
-#################################################################
-#################################################################
-
 try {
     if ($_POST) {
         $c_tickets = 0;
